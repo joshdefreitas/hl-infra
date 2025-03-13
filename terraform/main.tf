7436ab092ab76423bdfcdb8008b30e861fc7f02c
Josh De Freitas <joshdefreitas@gmail.com>
2025-03-13 10:07:37 -0400

    added disk config terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,31 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source = "telmate/proxmox"
-      version = "3.0.1-rc3"
-    }
-  }
-}
-
-provider "proxmox" {
-  pm_api_url = var.proxmox_api_url
-  pm_api_token_id = var.proxmox_api_token_id
-  pm_api_token_secret = var.proxmox_api_token_secret
-  pm_tls_insecure = true
-}
-
 resource "proxmox_vm_qemu" "test_vm" {
   name = var.vm_name
   target_node = var.proxmox_node
   
-  # If template_name is a name (string), this is fine
-  # If it's a numeric ID, make sure var.template_name is defined as a number
   clone = var.template_name
   
-  # VM settings - these look good as numbers
   cores = 2
   sockets = 1
   memory = 2048
   
-  # Add agent parameter explicitly as a number
   agent = 1
+  
+  # Add disk configuration
+  disk {
+    type = "scsi"
+    storage = "local-lvm"  # Replace with your actual storage pool name
+    size = "20G"
+    scsihw = "virtio-scsi-pci"
+  }
+  
+  # Set boot order
+  boot = "c"  # Boot from hard disk first
   
   # Network
   network {
     bridge = "vmbr0"
     model = "virtio"
-    # Explicitly set any numeric parameters
-    tag = -1  # If not using VLAN tagging
+    tag = -1
   }
   
   # Cloud-init settings

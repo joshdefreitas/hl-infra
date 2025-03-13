cc08c74b0c743a8e9dd632f78c408be43b0b4415
Josh De Freitas <joshdefreitas@gmail.com>
2025-03-13 10:15:06 -0400

    added back provider details

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,19 @@
+terraform {
+   required_providers {
+     proxmox = {
+       source = "telmate/proxmox"
+       version = "3.0.1-rc3"
+     }
+   }
+ }
+ 
+ provider "proxmox" {
+   pm_api_url = var.proxmox_api_url
+   pm_api_token_id = var.proxmox_api_token_id
+   pm_api_token_secret = var.proxmox_api_token_secret
+   pm_tls_insecure = true
+ }
+
 resource "proxmox_vm_qemu" "test_vm" {
   name = var.vm_name
   target_node = var.proxmox_node
